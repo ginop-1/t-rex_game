b3c7105d0d2e17e41aa7cd1cf7e56359c5e0878c
ginop-1 <enrico.covili@fermi.mo.it>
2020-09-20 18:30:13 +0200

    obstacles working, cleaning code

--- t-rex_game.cpp
+++ t-rex_game.cpp
@@ -2,15 +2,12 @@
  * "T-rex Game" (That little game that comes out with chrome
  * when you are offline) 
  * 
- * By Peppe289
- *
+ * By Peppe289 and GinoP
  *
  * Use WASD for game -> getch()
  * Use int matrix for script.
  * 
-*/
-
-// ATTUALMENTE IL PROGETTO E' IN FASE SPERIMENTALE, IL LAVORO TERMINATO SARA' DIVERSO 
+ ATTUALMENTE IL PROGETTO E' IN FASE SPERIMENTALE, IL LAVORO TERMINATO SARA' DIVERSO */
 
 #include <iostream>
 #include <time.h>
@@ -23,23 +20,20 @@
 #include <thread>
 
 #define LENGTH_MATRIX 50    // lunghezza d'azione 
-#define HEIGHT_MATRIX 10     // altezza d'azione  
-#define SIZE_VET 5          // limite massimo di ostacoli 
+#define HEIGHT_MATRIX 10    // altezza d'azione  
 
 struct GAME{
     // Il valore dell'int verrà elaborato per dare l'immagine 
-    int MATRIX [ HEIGHT_MATRIX ] [ LENGTH_MATRIX ];                           // use WASD 
+    int MATRIX [ HEIGHT_MATRIX ] [ LENGTH_MATRIX ];     // use WASD 
     // incrementa ad ogni passo, ed incrementa la velocità ad ogni soglia 
         int input[2] = {0 ,4}; 
     int punti = 0;
-    int riposiziona[ HEIGHT_MATRIX ];                   //Memorizza le posizioni per lo spostamento
 };
 
 // Valore della matrice dell'int 
 #define CLEAR 0     // vuoto 
 #define t_rex 1     // personaggio 
 #define OST 2       // ostacolo 
-#define START 0     // posizione di inizio 
 
 //Color set
 const std::string blue("\033[0;34m");
@@ -50,6 +44,7 @@ const std::string reset("\033[0m");
 GAME GRAFICA;
 
 // Prototipo di funzione 
+void firma();                   // Presentazione dei creatori
 void clear();                   // pulisce la matrice
 char interprete(int, int);      // elabora char 
 void clearCONSOLE();            // clean previous console output for creating a single game
@@ -63,8 +58,26 @@ void sistem_pause_medium();
 void sistem_pause_long();
 
 // Posiziona il personaggio nel posizione iniziale 
-void inizializza_matrix();
-void input_WASD();              // Acquisisci input 
+bool inizializza_matrix();
+bool input_WASD();              // Acquisisci input 
+
+//***********************************************************
+
+int main()
+{
+    firma();
+    clear();
+    while(true)
+    {
+        if(inizializza_matrix() == false) break;
+        print();
+        std::cout<<"\n";
+        scorrimento();
+    }
+
+    std::cout<<red<<"\nGame Over!\n";
+    return 0;
+}
 
 void firma()
 {
@@ -79,66 +92,67 @@ void firma()
     std::this_thread::sleep_for(std::chrono::seconds(3));
 }
 
-//***********************************************************
-
-int main()
-{
-    firma();
-    clear();
-    while(true)
-    {
-        inizializza_matrix();
-        print();
-        std::cout<<"\n";
-        scorrimento();
-    }
-
-    std::cout<<"\n";
-    return 0;
-}
-
-/*
- * Fixed scorrimento.
- * Clear matrice, interprete, print, scorrimento, posiziona l'ostacolo.
- */
-
-void scorrimento()
-{
-    int temp;
-    int rand;
-    ++GRAFICA.punti; //Punteggio
-
-    // Altezza (10) 
-    for(int i = 0; i != HEIGHT_MATRIX; ++i)
-    {
-        // Lunghezza (50)
-        for(int j = 0; j != LENGTH_MATRIX; ++j)
-        {
-            temp = GRAFICA.MATRIX[i][j];
-            GRAFICA.MATRIX[i][j-1] = temp;
-        }
-    }
-    
-    //Wipe dell'ultima cella per allorcazione dell'ostacolo
-    for(int i = 0; i != HEIGHT_MATRIX; ++i)
-        GRAFICA.MATRIX[ i ][ LENGTH_MATRIX -1 ] = CLEAR ;
-
-    rand = ostacolo();
-    if(rand<9) GRAFICA.MATRIX[ rand ][ LENGTH_MATRIX-1 ] = OST;
-}
-
-int ostacolo()   
-{
-    srand(time(0));
-    return rand()%15+1;
-}
-
 // wipe 
 void clear()
 {
     for(int i=0; i != HEIGHT_MATRIX; ++i)
         for(int j=0; j != LENGTH_MATRIX; ++j)
             GRAFICA.MATRIX[i][j]=CLEAR;
+}
+
+bool inizializza_matrix()
+{
+    if( GRAFICA.punti != 0 )
+    {
+        if (input_WASD() == false) return false;
+    }
+    else
+    {
+        GRAFICA.MATRIX[ 4 ][ 3 ] = t_rex;
+        return true;
+    }
+    return true;
+}
+
+bool input_WASD()
+{
+    char in = ' ';
+    GRAFICA.input[0] = GRAFICA.input[1];
+    bool risp=true;
+    while (true) 
+    {
+        while (true)
+        {
+        std::cin>>in;
+        if (in == 'w' || in == 's') break;
+        }
+        if( in != 'w' )
+        {
+            if (GRAFICA.input [1] != HEIGHT_MATRIX - 1)
+            ++GRAFICA.input[1];
+        }
+        else
+        {
+            if(GRAFICA.input[1] != 1)
+            --GRAFICA.input[1];
+        }
+        GRAFICA.MATRIX[ GRAFICA.input[ 0 ] ][ 3 ] = CLEAR;                      // clear the previous block
+        if (GRAFICA.MATRIX[ GRAFICA.input[ 1 ] ][ 4 ] == OST) return false;     // Check if actual block is an obstacle
+        else 
+        {
+            GRAFICA.MATRIX[ GRAFICA.input[ 1 ] ][ 3 ] = t_rex;
+            return true;
+        }
+    }
+}
+
+void clearCONSOLE()
+{
+    #if defined _WIN32
+        system("cls");
+    #else 
+        system("clear");
+    #endif
 }
 
 //*********** Elabora il print ***********
@@ -154,15 +168,6 @@ char interprete(int x, int y)
         return '|'; // crea gli ostacoli 
     
     return ' ';
-}
-
-void clearCONSOLE()
-{
-    #if defined _WIN32
-        system("cls");
-    #else 
-        system("clear");
-    #endif
 }
 
 void print()
@@ -182,45 +187,31 @@ void print()
         std::cout<<"-";
 }
 
-//************** Crea lo script del personaggio *************
-/*
- * Il presonaggio verrà posizionato nella seconda colonna.
- * Inizializzato alla 4 riga
- */
- 
-void inizializza_matrix()
+void scorrimento()
 {
-    if( GRAFICA.punti != 0 )
-        input_WASD();
-    else
-        GRAFICA.MATRIX[ 4 ][ 3 ] = t_rex;
+    int temp;
+    int obstacle;
+    ++GRAFICA.punti; //Punteggio
+
+    // Altezza (10) 
+    for(int i = 0; i != HEIGHT_MATRIX; ++i)
+    {
+        // Lunghezza (50)
+        for(int j = 0; j != LENGTH_MATRIX; ++j)
+        {
+            temp = GRAFICA.MATRIX[i][j];
+            GRAFICA.MATRIX[i][j-1] = temp;
+        }
+    }
+    //Wipe dell'ultima cella per allorcazione dell'ostacolo
+    for(int i = 0; i != HEIGHT_MATRIX; ++i)
+        GRAFICA.MATRIX[ i ][ LENGTH_MATRIX -1 ] = CLEAR ;
+
+    // generate obstacles randomly
+    srand(time(0));
+    obstacle = rand()%15+1;
+    if(obstacle<9) GRAFICA.MATRIX[ obstacle ][ LENGTH_MATRIX-1 ] = OST;
 }
 
-//fix movimento
-void input_WASD()
-{
-    char in;
-    GRAFICA.input[0] = GRAFICA.input[1];
-    //temp = GRAFICA.input[ 1 ];
-    //GRAFICA.input[ 0 ] = temp;
-    bool risp=true;
-    while (risp) 
-    {
-        std::cin>>in;
-        if(in == 'w' || in == 's')
-            if( GRAFICA.input[ 1 ] < HEIGHT_MATRIX && GRAFICA.input[ 1 ] > 0 ) risp = false;
-    }
 
-    if( in != 'w' )
-    {
-        if (GRAFICA.input [1] != HEIGHT_MATRIX - 1)
-        ++GRAFICA.input[1];
-    }
-    else
-    {
-        if(GRAFICA.input[1] != 1)
-        --GRAFICA.input[1];
-    }
-    GRAFICA.MATRIX[ GRAFICA.input[ 0 ] ][ 3 ] = CLEAR;
-    GRAFICA.MATRIX[ GRAFICA.input[ 1 ] ][ 3 ] = t_rex;
-}
+
